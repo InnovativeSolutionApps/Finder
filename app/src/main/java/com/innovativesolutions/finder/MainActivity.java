629c3d865b8be132c7c73472feb071bbc565d921
InnovativeSolutionApps <80323765+InnovativeSolutionApps@users.noreply.github.com>
2021-03-10 22:11:00 +0530

    10 Mar 2021 - by sakthi
    1.Left Nav bar created,
    2. center Grid view integrated.
    all are blue print only, Need to make it more advance.

--- app/src/main/java/com/innovativesolutions/finder/MainActivity.java
+++ app/src/main/java/com/innovativesolutions/finder/MainActivity.java
@@ -1,14 +1,160 @@
 package com.innovativesolutions.finder;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import java.io.File;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener {
+
+    private static final int REQUEST_PATH = 1;
+    GridView grid;
+    private File currentDir;
+
+    private com.innovativesolutions.finder.FileArrayAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drawer_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // launch app in Landscape more
+
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        grid = (GridView) findViewById(R.id.grid);
+        currentDir = new File("/sdcard/");
+        fill(currentDir);
+
+
     }
+
+
+    private void fill(File f) {
+
+        TextView absolutePath = findViewById(R.id.filePath);
+        File[] dirs = f.listFiles();
+        absolutePath.setText("Current Dir: " + f.getAbsolutePath());
+        List<Item> dir = new ArrayList<Item>();
+        List<com.innovativesolutions.finder.Item> fls = new ArrayList<com.innovativesolutions.finder.Item>();
+        try {
+            for (File ff : dirs) {
+                Date lastModDate = new Date(ff.lastModified());
+                DateFormat formater = DateFormat.getDateTimeInstance();
+                String date_modify = formater.format(lastModDate);
+                if (ff.isDirectory()) {
+
+
+                    File[] fbuf = ff.listFiles();
+                    int buf = 0;
+                    if (fbuf != null) {
+                        buf = fbuf.length;
+                    } else buf = 0;
+                    String num_item = String.valueOf(buf);
+                    if (buf == 0) num_item = num_item + " item";
+                    else num_item = num_item + " items";
+
+                    //String formated = lastModDate.toString();
+                    dir.add(new com.innovativesolutions.finder.Item(ff.getName(), num_item, date_modify, ff.getAbsolutePath(), "directory_icon"));
+                } else {
+
+                    fls.add(new com.innovativesolutions.finder.Item(ff.getName(), ff.length() + " Byte", date_modify, ff.getAbsolutePath(), "file_icon"));
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        Collections.sort(dir);
+        Collections.sort(fls);
+        dir.addAll(fls);
+        if (!f.getName().equalsIgnoreCase("sdcard"))
+            dir.add(0, new com.innovativesolutions.finder.Item("..", "Parent Directory", "", f.getParent(), "directory_up"));
+        adapter = new com.innovativesolutions.finder.FileArrayAdapter(com.innovativesolutions.finder.MainActivity.this, R.layout.file_view, dir);
+
+
+        grid.setAdapter(adapter);
+        grid.setOnItemClickListener(this);
+        System.out.println("");
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        com.innovativesolutions.finder.Item o = adapter.getItem(position);
+        if (o.getImage().equalsIgnoreCase("directory_icon") || o.getImage().equalsIgnoreCase("directory_up")) {
+            currentDir = new File(o.getPath());
+            fill(currentDir);
+            //Toast.makeText(this, "Folder Clicked: "+ currentDir, Toast.LENGTH_SHORT).show();
+        } else {
+            //onFileClick(o);
+        }
+
+    }
+
+
+    private void onFileClick(com.innovativesolutions.finder.Item o) {
+        Toast.makeText(this, "File Clicked: " + currentDir, Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+
+            System.out.println("+++++++++++++++++++");
+
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // drawer.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+
 }
